b29a81c2397b309b72d1acc9576bd0fc0e5832ea
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-06-05 12:25:55 +0200

    feat: add ram and storage usage

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,4 +1,6 @@
 import osUtils from 'os-utils';
+import fs from 'fs';
+
 const POLLING_INTERVAL = 500;
 
 function getCpuUsage() {
@@ -7,9 +9,30 @@ function getCpuUsage() {
   });
 }
 
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
+}
+
 export function pollResources() {
   setInterval(async () => {
     const cpuUsage = await getCpuUsage();
-    console.log(cpuUsage);
+    const ramUsage = getRamUsage();
+    const storageData = getStorageData();
+    console.log(`
+[CPU] ${cpuUsage}
+[RAM] ${ramUsage}
+[Storage] ${storageData.usage} / ${storageData.total}
+`);
   }, POLLING_INTERVAL);
 }
