c1535ec071b7542018e60f994d45a229c28bc6fd
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-06-05 13:29:46 +0200

    feat: finish console resource manager

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,27 +1,59 @@
 import osUtils from 'os-utils';
 import fs from 'fs';
+import os from 'os';
 
 const POLLING_INTERVAL = 500;
 
-function getCpuUsage() {
+function getCpuUsage(): Promise<number> {
   return new Promise(res => {
     osUtils.cpuUsage(res);
   });
 }
 
 function getRamUsage() {
-  return 1 - osUtils.freememPercentage();
+  const total = os.totalmem();
+  const free = os.freemem();
+  const used = total - free;
+
+  const usedGB = Math.floor(used / 1_000_000_000);
+  const totalGB = Math.floor(total / 1_000_000_000);
+
+  return {
+    percent: used / total,
+    usedGB,
+    totalGB,
+  };
 }
 
 function getStorageData() {
   const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
   const total = stats.bsize * stats.blocks;
   const free = stats.bsize * stats.bfree;
+  const used = total - free;
 
   return {
     total: Math.floor(total / 1_000_000_000),
+    used: Math.floor(used / 1_000_000_000),
     usage: 1 - free / total,
   };
+}
+
+function getStaticData() {
+  const totalStorage = getStorageData().total;
+  const cpuModel = os.cpus()[0].model;
+  const totalRAMGB = Math.floor(osUtils.totalmem() / 1024);
+  return {
+    totalStorage,
+    cpuModel,
+    totalRAMGB,
+  };
+}
+
+const INDENT = '      ';
+
+//prettier-ignore
+function formatLine(label: string, value: string, extra: string = '') {
+  return `${label.padEnd(10)}: ${value.padStart(6)} ${extra}`.trimEnd();
 }
 
 export function pollResources() {
@@ -29,10 +61,16 @@ export function pollResources() {
     const cpuUsage = await getCpuUsage();
     const ramUsage = getRamUsage();
     const storageData = getStorageData();
-    console.log(`
-[CPU] ${cpuUsage}
-[RAM] ${ramUsage}
-[Storage] ${storageData.usage} / ${storageData.total}
-`);
+    const staticData = getStaticData();
+
+    // const usagePercent = Math.floor(storageData.usage * 100);
+
+    // prettier-ignore
+    console.log(`\n${INDENT}System Monitor
+      ==============
+      ${formatLine('CPU', `${Math.round(cpuUsage * 100)}%`, `(${staticData.cpuModel})`)}
+      ${formatLine('RAM', `${Math.round(ramUsage.percent * 100)}%`, `(${ramUsage.usedGB} / ${ramUsage.totalGB} GB)`)}
+      ${formatLine('Storage', `${Math.floor(storageData.usage * 100)}%`, `${storageData.used} / ${storageData.total} GB`)}
+      `);
   }, POLLING_INTERVAL);
 }
